6ba7c299d70339283bb3f9aa3501555b11738a6c
KevJin123 <77636228+KevJin123@users.noreply.github.com>
2025-03-25 11:52:35 -0400

    Extracted hourly forecast arrays and flattened to single array. Will add to HourlyForecast comp

---
+++ react-projects/WeatherApp/src/components/HourlyForecast.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function HourlyForecast({cardData, isCelsius}) {
+    
+    // const [today, tomorrow] = cardData
+    console.log(cardData)
+
+    // const tempDisplay = 
+
+    // The game plan is to show the hourly forecast of current day to 24 hours ahead of right now
+    // which means we'll need two forecast arrays, index 0 and 1 where index 0 is the forecast of
+    // the day and index 1 is the forecast of the day after. Each "hour" array has 24 elements, but
+    // we only want to display from the hour after the most recent hour--if it is 10 AM right now, then
+    // the hourly forecast starts from 11 AM, because 10 AM is going to be current weather--to 24 hours
+    // from right now, so 11 AM to 10 AM (next day).
+
+    // We'll need some checks to make sure that we either cut off the hourly array or pick the index after
+    // the current hour.
+
+  return (
+    // The array that held two separate arrays with hourly forecast data have been flattened to a single array.
+    // TO DO:
+    // Loop through the single array and display those elements in a single div.
+    <div>HourlyForecast</div>
+  )
+}
+
+export default HourlyForecast
--- react-projects/WeatherApp/src/components/WeatherDisplay.jsx
+++ react-projects/WeatherApp/src/components/WeatherDisplay.jsx
@@ -1,10 +1,14 @@
 import React, {useState} from 'react'
 import WeatherCard from './WeatherCard'
 import ForecastCard from './ForecastCard'
+import HourlyForecast from './HourlyForecast'
 
 function DisplayWeather({forecastWeather, weatherError}) {
 
+  const currTime = new Date().getHours();
+
   let forecastArray = []
+  let hourlyForecast = []
 
   const [isCelsius, setIsCelsius] = useState(true)
 
@@ -23,6 +27,12 @@ function DisplayWeather({forecastWeather, weatherError}) {
       forecastArray = forecastWeather.forecast.forecastday;
       // console.log(forecastArray)
       // forecastArray.map((item, index) => console.log(item.date, index))
+      hourlyForecast = forecastWeather.forecast.forecastday.slice(0, 2)
+      .map((elem, index) => 
+        {if (index === 0) {return elem.hour.slice(currTime)} 
+      else {return elem.hour.slice(0, currTime + 1)}})
+      .flat();
+      // console.log(hourlyForecast)
     }
 
    
@@ -33,11 +43,13 @@ function DisplayWeather({forecastWeather, weatherError}) {
       {/* TO DO:
         Going to add hourly forecast now.
       */}
+      <h2>Hourly Forecast</h2>
+      {/* {hourlyForecast? hourlyForecast.map((element) => <HourlyForecast cardData={element} isCelsius={isCelsius} /> ) : "No hourly forecast"} */}
+      {/* // <HourlyForecast cardData={hourlyForecast} isCelsius={isCelsius} /> */}
       <h2>Daily Forecast</h2>
       {forecastArray ? forecastArray.map((element, index) => <ForecastCard key={index} isCelsius={isCelsius} cardData={element} />) : "No forecast"}
       
     </>
-    // <div>Current temperature in Fahrenheit in {weatherObj.location.name} is {weatherObj.current.temp_f}</div>
     
   )
 }
