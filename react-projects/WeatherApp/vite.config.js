c1e294db6b2252e46d32aae33b60b2c8ea1dbb18
KevJin123 <77636228+KevJin123@users.noreply.github.com>
2025-07-05 02:42:49 -0400

    multiple apps deployment

--- react-projects/WeatherApp/vite.config.js
+++ react-projects/WeatherApp/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/personal-projects/"
+  base: "/personal-projects/WeatherApp",
+  build: {
+    outDir: "dist"
+  }
 })
